bc5e07dced939691b244b973d3b0787e83f98c9b
yanil8068 <yanil8068@gmail.com>
2024-09-01 12:49:04 +0530

    deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Default output directory
+    sourcemap: true, // Generate source maps for debugging
+  },
+  // If your app is served from a subdirectory on the domain, set the base path
+  base: "/", // Default is '/' (root of domain)
+});
